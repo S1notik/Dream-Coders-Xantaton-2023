5949e3d387d3ded5b9037777cf966c4642157e97
cena11111 <89628106+cena11111@users.noreply.github.com>
2023-11-22 22:51:07 +0500

    Changed inlinekeyboard in infografika

--- keyboards/inline/__init__.py
+++ keyboards/inline/__init__.py
@@ -1,3 +1,3 @@
 from .inline_buttons import types_materials_inline_keyboard
 from .inline_buttons import useful_links_inline_keyboard
-from .inline_buttons import interesting_facts_inline_keyboard
+from .inline_buttons import get_infography_keyboard
