ed74335167c582e9f9f7f7ef5bbe1e6f52000bf9
cena11111 <89628106+cena11111@users.noreply.github.com>
2023-11-22 22:43:49 +0500

    Completely  infografika

--- handlers/users/infografika.py
+++ handlers/users/infografika.py
@@ -6,15 +6,31 @@ from loader import dp
 
 @dp.message(F.text == "Инфографика📑")
 async def infografika(message: types.Message):
-    text = message.text
-
-    await message.answer("Инфографика - это то то")
+    await message.answer("Здесь вы можете узнать о переработке разных материалов")
 
 @dp.message(F.text == "Батарейки")
-async def bat(message: types.Message):
+async def battery(message: types.Message):
     await message.answer_photo("AgACAgIAAxkBAAPEZV4UbksK2D8JqkjISv0GfxGHjyMAAvzRMRvhWfFKEo1qNoy4V5YBAAMCAANzAAMzBA")
 
+@dp.message(F.text == "Композитная упаковка")
+async def сomposite_package(message: types.Message):
+    await message.answer_photo("AgACAgIAAxkBAAPSZV4WlLjLAcdBTD1rgVJrNL1dhDoAAg7SMRvhWfFKzblwFjsP9pUBAAMCAANzAAMzBA")
 
+@dp.message(F.text == "Металл")
+async def metal(message: types.Message):
+    await message.answer_photo("AgACAgIAAxkBAAPWZV4XPXa214zyQL2pz-io3jo5EdoAAhTSMRvhWfFKGNT50tu4XlMBAAMCAANzAAMzBA")
+
+@dp.message(F.text == "Пластик")
+async def plastic(message: types.Message):
+    await message.answer_photo("AgACAgIAAxkBAAPYZV4Xz6CCihsJT1TscsuOzHKkzlMAAhXSMRvhWfFKmZxSPBFCbx8BAAMCAANzAAMzBA")
+
+@dp.message(F.text == "Стекло")
+async def glass(message: types.Message):
+    await message.answer_photo("AgACAgIAAxkBAAPaZV4YBuDHB8O4wRr15hUjSjukjosAAhjSMRvhWfFKtIZOrpWMF1kBAAMCAANzAAMzBA")
+
+@dp.message(F.text == "Стекло")
+async def environmental_friendliness_of_packaging(message: types.Message):
+    await message.answer_photo("AgACAgIAAxkBAAPcZV4YDU5ujdZCY4vk-7lAC3IIEEEAAhnSMRvhWfFKQEN4sBpwo4IBAAMCAANzAAMzBA")
 
     '''if text == "Батарейки":
         await message.answer_photo("AgACAgIAAxkBAAPEZV4UbksK2D8JqkjISv0GfxGHjyMAAvzRMRvhWfFKEo1qNoy4V5YBAAMCAANzAAMzBA")
