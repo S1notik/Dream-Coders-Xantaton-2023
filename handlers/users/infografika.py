ea9ed6190da7f481b3d1e5aa38b1fb9c285700cc
Onrefy <tkovlev_nikita_5@mail.ru>
2023-11-22 20:04:28 +0500

    It's works

--- handlers/users/infografika.py
+++ handlers/users/infografika.py
@@ -1,9 +1,21 @@
 from aiogram import types, F
 from loader import dp, bot
 from aiogram.types import InputFile, ContentType, FSInputFile
+from aiogram.methods import SendPhoto
 
 
 @dp.message(F.text)
 async def infografika(message: types.Message):
     if message.text == "Батарейки":
-        await message.answer_photo(FSInputFile('Плакат_Батарейки.jpg'))
+        await message.answer_photo("AgACAgIAAxkBAAPEZV4UbksK2D8JqkjISv0GfxGHjyMAAvzRMRvhWfFKEo1qNoy4V5YBAAMCAANzAAMzBA")
+    elif message.text == "Композитная упаковка":
+        await message.answer_photo("AgACAgIAAxkBAAPSZV4WlLjLAcdBTD1rgVJrNL1dhDoAAg7SMRvhWfFKzblwFjsP9pUBAAMCAANzAAMzBA")
+    elif message.text == "Металл":
+        await message.answer_photo("AgACAgIAAxkBAAPWZV4XPXa214zyQL2pz-io3jo5EdoAAhTSMRvhWfFKGNT50tu4XlMBAAMCAANzAAMzBA")
+    elif message.text == "Пластик":
+         await message.answer_photo("AgACAgIAAxkBAAPYZV4Xz6CCihsJT1TscsuOzH"
+                                    "KkzlMAAhXSMRvhWfFKmZxSPBFCbx8BAAMCAANzAAMzBA")
+    elif message.text == "Стекло":
+        await message.answer_photo("AgACAgIAAxkBAAPaZV4YBuDHB8O4wRr15hUjSjukjosAAhjSMRvhWfFKtIZOrpWMF1kBAAMCAANzAAMzBA")
+    elif message.text == "Экологичность упаковки":
+        await message.answer_photo("AgACAgIAAxkBAAPcZV4YDU5ujdZCY4vk-7lAC3IIEEEAAhnSMRvhWfFKQEN4sBpwo4IBAAMCAANzAAMzBA")
