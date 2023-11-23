7d414a7b54c97384d5b552f217768a77a702e10e
cena11111 <89628106+cena11111@users.noreply.github.com>
2023-11-23 10:35:04 +0500

    Types of materials are done

--- handlers/users/types_materials.py
+++ handlers/users/types_materials.py
@@ -1,16 +1,18 @@
 from aiogram import types
 from aiogram.filters import CommandStart
 from aiogram import Bot, Dispatcher, F
+
+from keyboards.inline import types_materials_inline_keyboard
 from loader import dp
 
 
 @dp.message(F.text == "Виды сырья🗑️")
-async def types_mat(message: types.Message):
-    await message.answer("В этом разжеле вы узнате о правилах приёма разлычных отходов")
+async def types_materials(message: types.Message):
+    await message.answer("В этом разжеле вы узнате о правилах приёма разлычных отходов", reply_markup=types_materials_inline_keyboard)
 
 
-@dp.callback_query(F.text == "Стеклянные бутылки и банки")
-async def glass_bottle(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "glass_bottles_and_jars_typematerial")
+async def glass_bottles_and_jars(callback: types.CallbackQuery):
     await callback.message.answer("Коричневые, зеленые и бесцветные стеклянные бутылки и банки:\n\n"
                          "-Баллы за 1 килограмм: 1\n\n"
                          "-Правила приема:\n"
@@ -18,8 +20,8 @@ async def glass_bottle(callback: types.CallbackQuery):
                          "\t✅ Необходимо сдавать по отдельности коричневое, зеленое и бесцветное стекло")
 
 
-@dp.callback_query(F.text == "Макулатура")
-async def pwaste_papper(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "waste_paper_typematerial")
+async def waste_paper(callback: types.CallbackQuery):
     await callback.message.answer("Макулатура:\n\n"
                          "-Баллы за 1 килограмм: 2\n\n"
                          "-Правила приема:\n"
@@ -27,8 +29,8 @@ async def pwaste_papper(callback: types.CallbackQuery):
                          "\t✅ Чистое, сложенное в коробки или перевязанное, без жирных пятен и остатков еды, без файлов и скрепок")
 
 
-@dp.callback_query(F.text == "Пластиковые ящики ПНД")
-async def plastic_box(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "HDPE_plastic_boxes_typematerial")
+async def HDPE_plastic_boxes(callback: types.CallbackQuery):
     await callback.message.answer("Пластиковые ящики ПНД:\n\n"
                          "-Баллы за 1 килограмм: 5\n\n"
                          "-Правила приема:\n"
@@ -36,16 +38,16 @@ async def plastic_box(callback: types.CallbackQuery):
                          "\t✅ Всех размеров, цветов и толщины")
 
 
-@dp.callback_query(F.text == "Батарейки")
-async def batt(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "batteries_typematerial")
+async def batteries(callback: types.CallbackQuery):
     await callback.message.answer("Батарейки:\n\n"
                         "-Баллы за 1 килограмм: 20\n\n"
                         "-Правила приема:\n"
                         "\t✅ Любых типов и брендов\n")
 
 
-@dp.callback_query(F.text == "Канистры ПНД, ПВД")
-async def kanistry(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "HDPE_LDPE_canisters_typematerial")
+async def HDPE_LDPE_canisters(callback: types.CallbackQuery):
     await callback.message.answer("Канистры ПНД, ПВД:\n\n"
                          "-Баллы за 1 килограмм: 5\n\n"
                          "-Правила приема:\n"
@@ -53,28 +55,28 @@ async def kanistry(callback: types.CallbackQuery):
                          "\t✅ Всех размеров и цветов, в том числе от моторного масла")
 
 
-@dp.callback_query(F.text == "Стретч-пленка ПВД")
-async def stretch_plenka(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "LDPE_Stretch_film_typematerial")
+async def LDPE_Stretch_film(callback: types.CallbackQuery):
     await callback.message.answer("Стретч-пленка ПВД:\n\n"
                          "-Баллы за 1 килограмм: 7\n\n"
                          "-Правила приема:\n"
                          "\t✅ Чистые, без остатков еды, жира и других загрязнений\n"
-                         "\t✅ Необходимо удалить скотч, наклейки"
+                         "\t✅ Необходимо удалить скотч, наклейки\n"
                          "\t✅ Принимается отдельно от других видов пленки")
 
 
-@dp.callback_query(F.text == "Пленка ПВД")
-async def plenka(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "LDPE_film_typematerial")
+async def LDPE_film(callback: types.CallbackQuery):
     await callback.message.answer("Пленка ПВД:\n\n"
                          "-Баллы за 1 килограмм: 7\n\n"
                          "-Правила приема:\n"
                          "\t✅ Чистые, без остатков еды, жира и других загрязнений\n"
-                         "\t✅ Необходимо удалить скотч, наклейки, срезать “застежки”"
+                         "\t✅ Необходимо удалить скотч, наклейки, срезать “застежки\n"
                          "\t✅ Плёнки можно сдавать вместе, не разделяя по видам")
 
 
-@dp.callback_query(F.text == "Крышки ПНД, ПВД, ПП")
-async def cap(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "HDPE_LDPE_PP_covers_typematerial")
+async def HDPE_LDPE_PP_covers(callback: types.CallbackQuery):
     await callback.message.answer("Крышки ПНД, ПВД, ПП:\n\n"
                          "-Баллы за 1 килограмм: 15\n\n"
                          "-Правила приема:\n"
@@ -82,8 +84,8 @@ async def cap(callback: types.CallbackQuery):
                          "\t✅ Только с маркировкой 2, 4, 5")
 
 
-@dp.callback_query(F.text == "Белые ПЭТ-бутылки от напитков")
-async def white_PAT_Bottle(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "white_PET_bottles_from_drinks_typematerial")
+async def white_PET_bottles_from_drinks(callback: types.CallbackQuery):
     await callback.message.answer("Белые ПЭТ-бутылки от напитков:\n\n"
                          "-Баллы за 1 килограмм: 5\n\n"
                          "-Правила приема:\n"
@@ -91,12 +93,29 @@ async def white_PAT_Bottle(callback: types.CallbackQuery):
                          "\t✅ Только с маркировкой 2, 4, 5")
 
 
-@dp.callback_query(F.text == "Пакеты фасовочные и пакеты-майки ПНД, ПВД")
-async def paking_and_tshirt_bags(callback: types.CallbackQuery):
+@dp.callback_query(F.data == "packing_packages_and_packagesTshirts_of_HDPE_LDPE_typematerial")
+async def packing_packages_and_packagesTshirts_of_HDPE_LDPE(callback: types.CallbackQuery):
     await callback.message.answer("Пакеты фасовочные и пакеты-майки ПНД, ПВД:\n\n"
                          "-Баллы за 1 килограмм: 7\n\n"
                          "-Правила приема:\n"
                          "\t✅ Чистые, без остатков еды, жира и других загрязнений\n"
                          "\t✅ Без скотча и наклеек")
+
+@dp.callback_query(F.data == "PET_bottles_from_beverages_and_vegetable_oil_typematerial")
+async def PET_bottles_from_beverages_and_vegetable_oil(callback: types.CallbackQuery):
+    await callback.message.answer("ПЭТ-бутылки от напитков и растительного масла:\n\n"
+                         "-Баллы за 1 килограмм: 5\n\n"
+                         "-Правила приема:\n"
+                         "\t✅ Чистые, спрессованные, с маркировкой\n"
+                         "\t✅ Крышки необходимо откручивать и сдавать отдельно")
+
+
+@dp.callback_query(F.data == "aluminum_cans_typematerial")
+async def aluminum_cans(callback: types.CallbackQuery):
+    await callback.message.answer("Алюминиевые банки:\n\n"
+                         "-Баллы за 1 килограмм: 10\n\n"
+                         "-Правила приема:\n"
+                         "\t✅ Чистые, спресованные")
+
 
 
--- keyboards/inline/inline_buttons.py
+++ keyboards/inline/inline_buttons.py
@@ -40,47 +40,65 @@ types_materials_inline_buttons = [
         [
             InlineKeyboardButton(
                 text="Стеклянные бутылки и банки",
-                callback_data="glass_bottles_and_jars_typematerial"),
+                callback_data="glass_bottles_and_jars_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Макулатура",
-                callback_data="waste_paper_typematerial"),
+                callback_data="waste_paper_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Пластиковые ящики ПНД",
-                callback_data="HDPE_plastic_boxes_typematerial"),
+                callback_data="HDPE_plastic_boxes_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Батарейки",
-                callback_data="batteries_typematerial"),
+                callback_data="batteries_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Канистры ПНД, ПВД",
-                callback_data="HDPE_LDPE_canisters_typematerial"),
+                callback_data="HDPE_LDPE_canisters_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Стретч-пленка ПВД",
-                callback_data="LDPE_Stretch_film_typematerial"),
+                callback_data="LDPE_Stretch_film_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Пленка ПВД",
-                callback_data="LDPE_film_typematerial"),
+                callback_data="LDPE_film_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Крышки ПНД, ПВД, ПП",
-                callback_data="HDPE_LDPE_PP_covers_typematerial"),
+                callback_data="HDPE_LDPE_PP_covers_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Белые ПЭТ-бутылки от напитков",
-                callback_data="white_PET_bottles_from_drinks_typematerial"),
+                callback_data="white_PET_bottles_from_drinks_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Пакеты фасовочные и пакеты-майки ПНД, ПВД",
-                callback_data="packing_packages_and_packagesTshirts_of_HDPE_LDPE_typematerial"),
+                callback_data="packing_packages_and_packagesTshirts_of_HDPE_LDPE_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="ПЭТ-бутылки от напитков и растительного масла",
-                callback_data="PET_bottles_from_beverages_and_vegetable_oil_typematerial"),
+                callback_data="PET_bottles_from_beverages_and_vegetable_oil_typematerial")
+        ],
+        [
             InlineKeyboardButton(
                 text="Алюминиевые банки",
                 callback_data="aluminum_cans_typematerial")
         ]
     ]
-types_materials_inline_keyboard = InlineKeyboardMarkup(inline_keyboard=types_materials_inline_buttons,
-                                resize_keyboard=True,
-                                one_time_keyboard=True,
-                                input_field_placeholder="Choice a button",
-                                selective=True)
+types_materials_inline_keyboard = InlineKeyboardMarkup(inline_keyboard=types_materials_inline_buttons)
 
 
 
@@ -90,6 +108,8 @@ interesting_facts_inline_buttons = [
             InlineKeyboardButton(
                 text="5 причин сортировать♻️",
                 callback_data="reasons_to_sort"),
+        ],
+        [
             InlineKeyboardButton(
                 text="Куда направляются отходы на переработку♻️",
                 callback_data="where_the_waste_is_sent_for_recycling")
@@ -107,19 +127,29 @@ infography_buttons = [
         [
             InlineKeyboardButton(
                 text="Батарейки",
-                callback_data="battery_infography"),
+                callback_data="battery_infography")
+        ],
+        [
             InlineKeyboardButton(
                 text="Композитная упаковка",
-                callback_data="сomposite_package_infography"),
+                callback_data="сomposite_package_infography")
+        ],
+        [
             InlineKeyboardButton(
                 text="Металл",
-                callback_data="metal_infography"),
+                callback_data="metal_infography")
+        ],
+        [
             InlineKeyboardButton(
                 text="Пластик",
-                callback_data="plastic_infography"),
+                callback_data="plastic_infography")
+        ],
+        [
             InlineKeyboardButton(
                 text="Стекло",
-                callback_data="glass_infography"),
+                callback_data="glass_infography")
+        ],
+        [
             InlineKeyboardButton(
                 text="Экологичность упаковки",
                 callback_data="environmental_friendliness_of_packaging_infography")
