e9b5fa37cdc4d711b22b36b0ae9f3339c40d9df9
S1notik <semn.osipov.2007@bk.ru>
2023-11-23 12:04:06 +0500

    add Image in interesting_facts.py

--- handlers/users/interesting_facts.py
+++ handlers/users/interesting_facts.py
@@ -2,6 +2,8 @@ import time
 
 from aiogram import F, types
 from aiogram.enums import ParseMode
+from aiogram.types import FSInputFile
+
 from loader import dp
 from keyboards.inline.inline_buttons import interesting_facts_inline_keyboard
 
@@ -13,6 +15,7 @@ async def interesting_facts(message: types.Message):
 
 @dp.callback_query(F.data == "where_the_waste_is_sent_for_recycling")  # КУДА НАПРАВЛЯЮТСЯ ОТХОДЫ НА ПЕРЕРАБОТКУ
 async def where_is_pereraborka(callback: types.CallbackQuery):
+    await callback.message.answer_photo(FSInputFile("data/image/map_of_city.png"))
     await callback.message.answer(
         "<b>Отходы принимаются и взвешиваются отдельно каждая фракция, далее отправляются на переработку.</b>"
         "<i>Некоторые фракции востребованы в Ханты-Мансийске. Есть предприятие по изготовлению тротуарной "
