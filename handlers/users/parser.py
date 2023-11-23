3a71a9c63f7795f3de845993f8eedfb810a51c20
cena11111 <89628106+cena11111@users.noreply.github.com>
2023-11-23 23:06:02 +0500

    Some changes in parser

--- handlers/users/parser.py
+++ handlers/users/parser.py
@@ -1,6 +1,6 @@
 import requests
+from bs4 import BeautifulSoup as BS
 from aiogram import F, types
-
 from keyboards.inline import link_for_post_inline_keyboard
 from loader import dp
 import os
@@ -8,10 +8,22 @@ import os
 
 @dp.message(F.text == "Посты")
 async def pars(message: types.Message):
+    request = requests.get("https://vk.com/eco4u2")
+    html = BS(request.content, "html.parser")
+    posts = html.select("//*[@id=\"page_wall_posts\"]")
+    current_num_of_posts = 0
+    if message.from_user.id == 1649811196:
+        while True:
+            request = requests.get("https://vk.com/eco4u2")
+            html = BS(request.content, "html.parser")
+
+
+
+async def send_post_to_telegram(message: types.Message):
     SECRET_TOKEN = str(os.getenv("SECRET_TOKEN"))
     version = 5.137
     domain = "eco4u2"
-    count = 2
+    count = 1
     offset = 1
     response = requests.get(
         "https://api.vk.com/method/wall.get",
