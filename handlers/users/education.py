7ea09a6708a72df9fae634f5bfd249c1b16d69a0
cena11111 <89628106+cena11111@users.noreply.github.com>
2023-11-23 22:28:57 +0500

    Customize text and fixing some bugs

--- handlers/users/education.py
+++ handlers/users/education.py
@@ -1,16 +1,20 @@
 from aiogram import types, F
+from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.types import FSInputFile
+
 from keyboards.inline import useful_links_inline_keyboard, ecolesson_inline_keyboard
 from loader import dp
 from keyboards.defoult.reply_buttons import education_keyboard
 from keyboards.inline import question1_keyboard, question2_keyboard
+import time
 
 
 
 @dp.message(F.text == "Обучающие материалы📚")
 async def education(message: types.Message):
-    await message.answer("Вы в обучающих материалах",
-                         reply_markup=education_keyboard)
+    await message.answer("<b>Вы окунулись в мир образовательных материалов</b>📚",
+                         reply_markup=education_keyboard, parse_mode=ParseMode.HTML)
 
 
 # Useful links and video lessons
@@ -23,10 +27,16 @@ async def education(message: types.Message):
 # Ссылка на экоурок и его описание
 @dp.message(F.text == "Экоурок🏫")
 async def education(message: types.Message):
-    await message.answer("Экоцентр «Югра Собирает» не только пункт приема вторсырья - это еще и образовательная площадка, на которой проходят лекции.\n\n"
-                         "На Экоуроке будет наглядно показано какие виды отходов перерабатываются или нет, какая альтернатива есть одноразовым пластиковым вещам и что изготавливается из вторичного сырья."
-                         " Посетители экоуроков узнают о раздельном сборе отходов и дальнейшем пути вторичного сырья из первых рук.",
-                         reply_markup=ecolesson_inline_keyboard)
+    await message.answer_photo(FSInputFile("data/image/logo_to_aco_lessons.jpg"))
+    await message.answer("Экоцентр «Югра Собирает» не только пункт приема вторсырья - это еще и образовательная площадка, на которой проходят лекции.\n\n")
+
+    time.sleep(1)
+
+    await message.answer(
+        "На Экоуроке будет наглядно показано <b>какие виды отходов перерабатываются</b> или нет, какая альтернатива есть одноразовым пластиковым вещам и что изготавливается из вторичного сырья."
+        " Посетители экоуроков узнают о <ins>раздельном сборе отходов и дальнейшем пути вторичного сырья</ins> из первых рук.",
+        reply_markup=ecolesson_inline_keyboard, parse_mode=ParseMode.HTML)
+
 
 # Тест на 10 вопросов
 
@@ -36,22 +46,22 @@ async def start_test_question1(message: types.Message):
                          reply_markup=question1_keyboard)
 
 @dp.callback_query(F.data == "answer1_question1_quiz")
-async def question2(message: types.Message):
-    await message.answer("Верно!\n\n"
+async def question2(callback: types.CallbackQuery):
+    await callback.message.answer("Верно!\n\n"
                          "Следующий вопрос:\n"
                          "Можно ли класть одноразовый стаканчик из-под кофе в контейнер для бумаги?",
                          reply_markup=question2_keyboard)
 
 @dp.callback_query(F.data == "answer2_question1_quiz")
-async def question2(message: types.Message):
-    await message.answer("Неверно!\n\n"
+async def question2(callback: types.CallbackQuery):
+    await callback.message.answer("Неверно!\n\n"
                          "Следующий вопрос:\n"
                          "Можно ли класть одноразовый стаканчик из-под кофе в контейнер для бумаги?",
                          reply_markup=question2_keyboard)
 
 @dp.callback_query(F.data == "answer3_question1_quiz")
-async def question2(message: types.Message):
-    await message.answer("Неверно!\n\n"
+async def question2(callback: types.CallbackQuery):
+    await callback.message.answer("Неверно!\n\n"
                          "Следующий вопрос:\n"
                          "Можно ли класть одноразовый стаканчик из-под кофе в контейнер для бумаги?",
                          reply_markup=question2_keyboard)
--- handlers/users/infografika.py
+++ handlers/users/infografika.py
@@ -1,4 +1,5 @@
 from aiogram import types, F
+from aiogram.enums import ParseMode
 from aiogram.types import FSInputFile
 
 from keyboards.inline.inline_buttons import infography_keyboard
@@ -7,61 +8,52 @@ from loader import dp
 
 @dp.message(F.text == "Инфографика📑")
 async def infografika(message: types.Message):
-    await message.answer("Здесь вы можете узнать о переработке разных материалов", reply_markup=infography_keyboard)
+    await message.answer("<b>В данном разделе вы можете узнать о переработке разных материалов</b>♻️",
+                         reply_markup=infography_keyboard, parse_mode=ParseMode.HTML)
 
 
 @dp.callback_query(F.data == "battery_infography")
 async def battery(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/battery_infograf.png"))
-    # await callback.message.answer_photo("AgACAgIAAxkBAAPEZV4UbksK2D8JqkjISv0GfxGHjyMAAvzRMRvhWfFKEo1qNoy4V5YBAAMCAANzAAMzBA")
+
 
 @dp.callback_query(F.data == "paper_infography")
 async def paper(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/paper_infograf.png"))
-    #await callback.message.answer_photo("AgACAgIAAxkBAAOkZV7NLDIt692t4wre8yXQjGLuM0EAAqPXMRvu6fBKcc_TTR3SeocBAAMCAANzAAMzBA")
-
 
 
 @dp.callback_query(F.data == "сomposite_package_infography")
 async def сomposite_package(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/composite_pack_infograf.png"))
-    # await callback.message.answer_photo("AgACAgIAAxkBAAPSZV4WlLjLAcdBTD1rgVJrNL1dhDoAAg7SMRvhWfFKzblwFjsP9pUBAAMCAANzAAMzBA")
 
 
 @dp.callback_query(F.data == "metal_infography")
 async def metal(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/metal_infograf.png"))
-    # await callback.message.answer_photo("AgACAgIAAxkBAAPWZV4XPXa214zyQL2pz-io3jo5EdoAAhTSMRvhWfFKGNT50tu4XlMBAAMCAANzAAMzBA")
 
 
 @dp.callback_query(F.data == "plastic_infography")
 async def plastic(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/plastic_infograf.png"))
-    # await callback.message.answer_photo("AgACAgIAAxkBAAPYZV4Xz6CCihsJT1TscsuOzHKkzlMAAhXSMRvhWfFKmZxSPBFCbx8BAAMCAANzAAMzBA")
 
 
 @dp.callback_query(F.data == "glass_infography")
 async def glass(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/glass_infograf.png"))
-    # await callback.message.answer_photo("AgACAgIAAxkBAAPaZV4YBuDHB8O4wRr15hUjSjukjosAAhjSMRvhWfFKtIZOrpWMF1kBAAMCAANzAAMzBA")
 
 
 @dp.callback_query(F.data == "environmental_friendliness_of_packaging_infography")
 async def environmental_friendliness_of_packaging(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/acolog_packaging_infograf.png"))
-    # await callback.message.answer_photo("AgACAgIAAxkBAAPcZV4YDU5ujdZCY4vk-7lAC3IIEEEAAhnSMRvhWfFKQEN4sBpwo4IBAAMCAANzAAMzBA")
-
-
 
 
 @dp.callback_query(F.data == "what_is_contain_bin_infography")
 async def what_is_contain_bin(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/what_is_contain_bin_infography_infograf.jpg"))
-    #await callback.message.answer_photo("AgACAgIAAxkBAAOxZV7R2KH7GmHgIP_sZi73HfHauSIAAq3XMRvu6fBKcMo08NIstu4BAAMCAANzAAMzBA")
+
 
 @dp.callback_query(F.data == "responsible_consumption_infography")
 async def responsible_consumption(callback: types.CallbackQuery):
     await callback.message.answer_photo(FSInputFile("data/image/responsible_consumption_infograf.jpg"))
-    #await callback.message.answer_photo("AgACAgIAAxkBAAOcZV7FhX9GKPaoAuEJKjwfyt2JbkYAApzXMRvu6fBKhfq0gE1coEMBAAMCAANzAAMzBA")
 
 
